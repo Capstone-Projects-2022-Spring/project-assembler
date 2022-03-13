41dfa7c653f714984ff9d9b0122f3f3ea7befae4
Chris Costa <tul15320@temple.edu>
2022-03-12 20:14:49 -0500

    working getfriends and addfriends feature linked to playfab account

--- Assets/scripts/UIManager.cs
+++ Assets/scripts/UIManager.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Serialization;
 using Mirror;
+using PlayFab;
+using PlayFab.ClientModels;
+using PlayFab.Json;
+using PlayFab.DataModels;
 
 public class UIManager : NetworkBehaviour
 {
@@ -115,7 +119,11 @@ public class UIManager : NetworkBehaviour
     }
 
     //-----------
-
+    /***
+     * Start of friends UI
+     */
+    void DisplayPlayFabError(PlayFabError error) { Debug.Log(error.GenerateErrorReport()); }
+    void DisplayError(string error) { Debug.LogError(error); }
     public void openFriendList(){
         FriendUI.gameObject.SetActive(true);
         mainMenuCanves.gameObject.SetActive(false);
@@ -124,6 +132,81 @@ public class UIManager : NetworkBehaviour
         FriendUI.gameObject.SetActive(false);
         mainMenuCanves.gameObject.SetActive(true);
     }
+
+    [SerializeField]
+    Transform friendScrollView;
+    List<FriendInfo> myFriends;
+    public GameObject FriendListing;
+    void DisplayFriends(List<FriendInfo> friendsCache) { 
+        foreach(FriendInfo friend in friendsCache){
+            bool isFound = false;
+            if(myFriends != null){
+                foreach (FriendInfo info in myFriends){
+                    if(friend.FriendPlayFabId == info.FriendPlayFabId)
+                        isFound = true;
+                }
+            }
+            if (isFound == false){
+                GameObject listing = Instantiate(FriendListing, friendScrollView);//variable used for leaderboard listings renamed
+                FriendList tempListing = listing.GetComponent<FriendList>();
+                tempListing.playerId.text = friend.TitleDisplayName;
+            }
+        } 
+        myFriends = friendsCache;
+    }
+
+    
+    //to get players current friends list data
+    //uses the GeTFriendsList API
+    List<FriendInfo> _friends = null;
+
+    public void GetFriends() {
+        PlayFabClientAPI.GetFriendsList(new GetFriendsListRequest {
+            IncludeSteamFriends = false,
+            IncludeFacebookFriends = false,
+            XboxToken = null
+        }, result => {
+            _friends = result.Friends;
+            DisplayFriends(_friends); // triggers your UI
+        }, DisplayPlayFabError);
+    }
+
+    string friendSearch;
+    [SerializeField]
+    GameObject friendPanel;
+
+    enum FriendIdType { PlayFabId, Username, Email, DisplayName };
+
+    void AddFriend(FriendIdType idType, string friendId) {
+        var request = new AddFriendRequest();
+        switch (idType) {
+            case FriendIdType.PlayFabId:
+                request.FriendPlayFabId = friendId;
+                break;
+            case FriendIdType.Username:
+                request.FriendUsername = friendId;
+                break;
+            case FriendIdType.Email:
+                request.FriendEmail = friendId;
+                break;
+            case FriendIdType.DisplayName:
+                request.FriendTitleDisplayName = friendId;
+                break;
+        }
+        // Execute request and update friends when we are done
+        PlayFabClientAPI.AddFriend(request, result => {
+            Debug.Log("Friend added successfully!");
+        }, DisplayPlayFabError);
+    }
+
+    public void InputFriendID(string idInput){
+        friendSearch = idInput;
+    }
+    public void SubmitFirendRequest(){
+        AddFriend(FriendIdType.PlayFabId, friendSearch);
+    }
+    //end friends UI 
+    
     /*
      * Game canves functions
      */
