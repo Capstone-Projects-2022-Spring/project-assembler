bec755be5efff21d121a20fbd719e970e3bd9bc2
Chris Costa <tul15320@temple.edu>
2022-03-23 20:33:28 -0400

    all UI elements assigned to proper areas

--- Assets/scripts/MapGenMenu.cs
+++ Assets/scripts/MapGenMenu.cs
@@ -5,58 +5,41 @@ using UnityEngine.UI;
 
 public class MapGenMenu : MonoBehaviour
 {
-    public Slider frequency;
+    public Slider RockFrequency, CopperFrequency, MetalFrequency, DirtFrequency, WaterFrequency, GrassFrequency, EnemyBaseFrequency;
+    public Slider RockRichness, CopperRichness, MetalRichness;
+    public Slider EnemyBaseSize, StaringAreaSize;
+    public InputField MaxGroup, MinGroup, MaxCooldown, MinCooldown, MaxExpansion, TimeFactor, DestroyFactor, PollutionFactor;
+    public GameObject TerrainLayout;
+    public GameObject ResourcesLayout;
+    public GameObject EnemyAiLayout;
+
     // Start is called before the first frame update
     void Start()
     {
-        frequency.onValueChanged.AddListener((v) => { 
+        RockFrequency.onValueChanged.AddListener((v) => { 
             Debug.Log(v.ToString("0.00"));
         });
-    }
-
-}
-/*
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-
-public class MapGenUI : MonoBehaviour
-{
-    public Text itemName;
-    public Slider frequency;
-    public Slider richness;
-    public GameObject Panel;
-    public GameObject Listing;
-    public Transform listingContainer;
-
-    void Start(){
-        frequency.onValueChanged.AddListener((v) => { 
+        RockRichness.onValueChanged.AddListener((v) => { 
             Debug.Log(v.ToString("0.00"));
         });
+        TerrainLayout.SetActive(true);
     }
-    
-    //three buttons if this one is clicked populate the list accoring to array of names 
-    //and previosuly saved scoller values 
-    //
-    //if ()
-    /*
-    String[] terrain = new string[2] {"Dirt", "Water", "Grass"};
-    String[] resources = new string[3] {"Metal", "Rock", "Copper", "Uranium"};
-    //String[] enemyAI = new string[3] { };
-
-    
     public void onTerrainClick(){
-        Panel.SetActive(true);
-        itemName = terrain[1];
+        TerrainLayout.SetActive(true);
+        ResourcesLayout.SetActive(false);
+        EnemyAiLayout.SetActive(false);
 
     }
-    public void onResourceClick(){
+    public void onResourcesClick(){
+        TerrainLayout.SetActive(false);
+        ResourcesLayout.SetActive(true);
+        EnemyAiLayout.SetActive(false);
 
     }
-    public void onEnemyAIClick(){
+    public void onEnemyAiClick(){
+        TerrainLayout.SetActive(false);
+        ResourcesLayout.SetActive(false);
+        EnemyAiLayout.SetActive(true);
 
     }
-
 }
-    */
