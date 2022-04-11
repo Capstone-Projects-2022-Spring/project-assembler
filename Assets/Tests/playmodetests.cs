960a4584e7486b18625cc85a68598a7afd018b80
max <majanney@me.com>
2022-04-10 17:34:46 -0700

    test private chat

--- Assets/Tests/playmodetests.cs
+++ Assets/Tests/playmodetests.cs
@@ -51,4 +51,31 @@ public class playmodetests : MonoBehaviour
         yield return null;
     }
 
+    [UnityTest, Order(3)]
+    public IEnumerator TestPrivateChatMessageWorking()
+    {
+        GameObject.Find("LoginCanves/LoginStuff/email").GetComponent<InputField>().text = "karim.salem@temple.edu";
+        GameObject.Find("LoginCanves/LoginStuff/password").GetComponent<InputField>().text = "testing";
+        GameObject.Find("LoginCanves/LoginStuff/LoginButton").GetComponent<Button>().onClick.Invoke();
+
+        // wait for chat-ui
+        float time = 0;
+        while (time < 10)
+        {
+            time += Time.fixedDeltaTime;
+            yield return new WaitForFixedUpdate();
+        }
+
+        // Simulate sending a message 
+        string message = "Testing private chat...";
+        ChatManager chatManager = GameObject.Find("UIscripts").GetComponent<ChatManager>();
+        chatManager.chatClient.SendPrivateMessage(chatManager.chatClient.UserId, message);
+
+
+        // check that our chat history ends with the message 
+        string history = chatManager.ChatHistory.text;
+        Assert.IsTrue(history.EndsWith(message), "Failed to send private message");
+
+        yield return null;
+    }
 }
